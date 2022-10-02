e5df675a83a825f73cd593957ec4a70ae70218b0
William Lingard <William.Lingard@kcl.ac.uk>
2022-10-02 23:02:55 +0100

    Initial prime code

--- primes.py
+++ primes.py
@@ -2,5 +2,18 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    primeList = []
+    nonPrimeList = []
+    
+    # Generate a list of non-primes
+    for i in range(2,8):
+        for j in range(i*2,number_of_primes*5,i):
+            nonPrimeList.append(j)
+
+    # Search throgh to find primes
+    x = 2
+    while len(primeList) < number_of_primes:
+        if x not in nonPrimeList:
+            primeList.append(x)
+        x+=1
+    return primeList
